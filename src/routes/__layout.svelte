3be24b461da5b08664dad381546e9842d032ab83
Kelvin Soh <kelvinsoh@pop-os.localdomain>
2022-02-14 11:52:49 +0800

    initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '../app.css';
+	import '../katex.min.css';
+	import '../mathlive-fonts.css';
+</script>
+
+<header class="text-center">
+	<a sveltekit:prefetch href="/">Home</a>
+</header>
+<main class="bg-stone-100">
+	<slot />
+</main>
+<footer>Footer</footer>
+
+<style>
+	main {
+		min-height: 100vh;
+		max-width: 100vw;
+	}
+	footer {
+		min-height: 50vw;
+		background: midnightblue;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import { math } from '$lib/math';
+	import FractionInput from '$lib/mathlive-inputs/FractionInput.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section class="flex flex-col justify-center items-center gap-4 py-4">
+	<a class="btn btn-primary" sveltekit:prefetch href="./fractions">Fraction Arithmetic</a>
+	<a class="btn btn-primary" sveltekit:prefetch href="./linear-equations/1a">Linear Equations 1A</a>
+	<a class="btn btn-primary" sveltekit:prefetch href="./linear-equations/1b">Linear Equations 1B</a>
+
+	<p>
+		{@html math('2x+3,y')} x
+	</p>
+
+	<FractionInput />
+</section>
